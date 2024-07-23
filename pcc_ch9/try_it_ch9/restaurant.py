8ec62925c403031c8b645e5e4137f1b1d8716ca2
Cuda <requin@csu.fullerton.edu>
2024-07-23 12:06:57 -0700

    Try it 9-4->5 fin. Tames mews.

--- pcc_ch9/try_it_ch9/restaurant.py
+++ pcc_ch9/try_it_ch9/restaurant.py
@@ -2,6 +2,7 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"Welcome to {self.restaurant_name}!")
@@ -9,6 +10,12 @@ class Restaurant:
 
     def open_restaurant(self):
         print(f"{self.restaurant_name} is now open!\n")
+
+    def set_number_served(self, customers):
+        self.number_served = customers
+
+    def increment_number_served(self, additional_customers):
+        self.number_served += additional_customers
 
 
 restaurant1 = Restaurant("Del Taco", "Mexican Fastfood")
@@ -22,3 +29,18 @@ restaurant2.open_restaurant()
 restaurant3 = Restaurant("Outback", "Steakhouse")
 restaurant3.describe_restaurant()
 restaurant3.open_restaurant()
+
+# 9-4 number served attribute modification
+restaurant4 = Restaurant("Islands", "Tropical Fusion")
+print(restaurant4.number_served)
+restaurant4.number_served = 10
+print(restaurant4.number_served)
+
+# attribute modification via method
+restaurant4.set_number_served(15)
+print(restaurant4.number_served)
+
+# attribute modification via incrementation
+restaurant4.increment_number_served(5)
+print(restaurant4.number_served)
+
--- pcc_ch9/try_it_ch9/users.py
+++ pcc_ch9/try_it_ch9/users.py
@@ -4,6 +4,7 @@ class User:
         self.last_name = last_name
         self.age = int(age)
         self.email = email
+        self.login_attempts = 0
 
     def describe_user(self):
         print("User information:")
@@ -14,6 +15,12 @@ class User:
 
     def greet_user(self):
         print(f"Welcome back, {self.first_name} {self.last_name}!\n")
+
+    def increment_login_attempts(self, logins):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
 
 user1 = User("Drew", "Croteau", 30, "requin@csu.fullerton.edu")
@@ -27,3 +34,12 @@ user2.greet_user()
 user3 = User("Bob", "Belcher", 46, "bbelcherburgers@goodeats.com")
 user3.describe_user()
 user3.greet_user()
+
+# Login Attempts 9-5 increment modification & reset
+user4 = User("Timothy", "Mewtron", 6, "shampooisbetta@catfax.com")
+for mews in range(0, 5):
+    user4.increment_login_attempts(mews)
+print(user4.login_attempts)
+
+user4.reset_login_attempts()
+print(user4.login_attempts)
